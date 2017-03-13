2d1d3cd3a4b1c02a420d8d5712ba2153af794bfc
ddandur <daniel.r.dandurand@gmail.com>
2017-03-13 16:51:16 -0700

    removed reference to import got, which imported python version of GetOldTweets

--- twords.py
+++ twords.py
@@ -24,11 +24,9 @@ from ttp import ttp
 import os, sys, inspect
 
 # use this if you want to include modules from a subfolder
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"GetOldTweets-python")))
-if cmd_subfolder not in sys.path:
-    sys.path.insert(0, cmd_subfolder)
-
-import got
+#cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"GetOldTweets-python")))
+#if cmd_subfolder not in sys.path:
+#    sys.path.insert(0, cmd_subfolder)
 
 pd.set_option('display.max_colwidth', -1)
 
